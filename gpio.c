51620e1447414279d191412bad0880f344a537ee
Jacob-Lemon <jrlemon03@gmail.com>
2024-11-15 10:50:36 -0700

    finished gpio.c and gpio.h. Needs testing still

--- gpio.c
+++ gpio.c
@@ -1,37 +1,83 @@
 #include "gpio.h"
-
 #include "stm32l476xx.h"
 
-
-
-void pinMode(GPIO_TypeDef *port, int pin, int mode) {
-    //port->MODER = ...................
-		port->MODER &= (mode << (pin*2));
+/******************************************************************************
+pin_mode will set the mode for a GPIO pin, like PA12.
+inputs:
+    pointer to a GPIO typedef
+    int - pin number
+    int - mode (0: Input, 1: Output, 2: Alternate Function, 3: Analog)
+outputs:
+    none
+example call:
+    pin_mode(GPIOA, 5, 1); // Configures PA5 as output mode
+******************************************************************************/
+void pin_mode(GPIO_TypeDef *port, int pin, int mode) {
+    port->MODER &= ~(0b11 << (pin * 2)); // Clear the 2 bits for the pin
+    port->MODER |= (mode << (pin * 2));  // Set the mode for the pin
 }
 
-void setOutputType(GPIO_TypeDef *port, int pin, int type) {
-    //port->OTYPER = ...................
+/******************************************************************************
+set_output_type sets the output type for a GPIO pin.
+inputs:
+    pointer to a GPIO typedef
+    int - pin number
+    int - type (0: Push-Pull, 1: Open-Drain)
+outputs:
+    none
+example call:
+    set_output_type(GPIOA, 5, 0); // Configures PA5 as Push-Pull output
+******************************************************************************/
+void set_output_type(GPIO_TypeDef *port, int pin, int type) {
+    port->OTYPER &= ~(1 << pin);  // Clear the bit for the pin
+    port->OTYPER |= (type << pin); // Set the type for the pin
 }
 
-void setPullUpDown(GPIO_TypeDef *port, int pin, int pupd) {
-    //port->PUPDR = ...................
+/******************************************************************************
+set_pull_up_down sets the pull-up or pull-down resistor for a GPIO pin.
+inputs:
+    pointer to a GPIO typedef
+    int - pin number
+    int - pupd (0: No Pull-Up/Pull-Down, 1: Pull-Up, 2: Pull-Down)
+outputs:
+    none
+example call:
+    set_pull_up_down(GPIOB, 3, 1); // Configures PB3 with Pull-Up resistor
+******************************************************************************/
+void set_pull_up_down(GPIO_TypeDef *port, int pin, int pupd) {
+    port->PUPDR &= ~(0b11 << (pin * 2)); // Clear the 2 bits for the pin
+    port->PUPDR |= (pupd << (pin * 2));  // Set the pull-up/pull-down configuration
 }
 
-void digitalWrite(GPIO_TypeDef *port, int pin, int value) {
-    //port->ODR = ...................
+/******************************************************************************
+digital_write sets the output state of a GPIO pin.
+inputs:
+    pointer to a GPIO typedef
+    int - pin number
+    int - value (0: Low, 1: High)
+outputs:
+    none
+example call:
+    digital_write(GPIOA, 5, 1); // Sets PA5 output to high
+******************************************************************************/
+void digital_write(GPIO_TypeDef *port, int pin, int value) {
+    if (value) {
+        port->ODR |= (1 << pin); // Set the pin
+    } else {
+        port->ODR &= ~(1 << pin); // Clear the pin
+    }
 }
 
-int digitalRead(GPIO_TypeDef *port, int pin) {
-    //return ...................
+/******************************************************************************
+digital_read reads the input state of a GPIO pin.
+inputs:
+    pointer to a GPIO typedef
+    int - pin number
+outputs:
+    int - value (0: Low, 1: High)
+example call:
+    int value = digital_read(GPIOB, 3); // Reads the input state of PB3
+******************************************************************************/
+int digital_read(GPIO_TypeDef *port, int pin) {
+    return (port->IDR & (1 << pin)) ? 1 : 0; // Read the pin state
 }
-/*
-int main() {
-    pinMode(GPIOA, 5, OUTPUT);
-    digitalWrite(GPIOA, 5, 1);
-    setOutputType(GPIOA, 5, OPENDRAIN);
-    pinMode(GPIOB, 3, INPUT);
-    setPullUpDown(GPIOB, 3, PULLUP)
-    int value = digitalRead(GPIOB, 3);
-}
-*/
-
--- gpio.h
+++ gpio.h
@@ -1,20 +1,27 @@
-#ifndef gpio_h
-#define gpio_h
-//-----macros-----
+#ifndef GPIO_H
+#define GPIO_H
+
+#include "stm32l476xx.h"
+
+//----- Macros -----
+// for use with pin_mode()
 #define OUTPUT 1
 #define INPUT 0
 
+// for use with set_output_type()
 #define PUSHPULL 0
 #define OPENDRAIN 1
 
+// for use with set_pull_up_down
 #define NONE 0
 #define PULLUP 1
 #define PULLDOWN 2
 
+//----- Function Prototypes -----
+void pin_mode(GPIO_TypeDef *port, int pin, int mode);
+void set_output_type(GPIO_TypeDef *port, int pin, int type);
+void set_pull_up_down(GPIO_TypeDef *port, int pin, int pupd);
+void digital_write(GPIO_TypeDef *port, int pin, int value);
+int digital_read(GPIO_TypeDef *port, int pin);
 
-//-----function prototypes-----
-
-
-
-
-#endif
+#endif // GPIO_H
